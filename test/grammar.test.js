33f438ab624d7d6874c190cd360c1c31c5380cb2
kirakira0 <kiratoal0@gmail.com>
2023-02-16 11:19:31 -0800

    Adding to grammar tests

--- test/grammar.test.js
+++ test/grammar.test.js
@@ -1,7 +1,35 @@
-import assert from "assert";
+import assert from "assert/strict";
+import fs from "fs";
+import ohm from "ohm-js";
 
-describe("The parser", () => {
-  it(`hello`, () => {
-    assert(1 === 1);
-  });
+// Programs expected to be syntactically correct
+const syntaxChecks = [
+  ["single line func", "func square(n) -> n<n>*"],
+  ["comments", "func square(n) -> n<n>* # Single line fundecl"],
+  ["variable assignment", "name<Vi> # Variable assignment"],
+  [
+    "print statements",
+    `print("Oh the misery, everybody wants to be my enemy")
+    print()`,
+  ],
+  ["func call", `myFunc(1, 3, "hello")`],
+];
+
+// Programs with syntax errors that the parser will detect
+const syntaxErrors = [["empty programs", "", /Line 1, col 1:/]];
+
+describe("The grammar", () => {
+  const grammar = ohm.grammar(fs.readFileSync("src/ekko.ohm"));
+  for (const [scenario, source] of syntaxChecks) {
+    it(`properly specifies ${scenario}`, () => {
+      assert(grammar.match(source).succeeded());
+    });
+  }
+  for (const [scenario, source, errorMessagePattern] of syntaxErrors) {
+    it(`does not permit ${scenario}`, () => {
+      const match = grammar.match(source);
+      assert(!match.succeeded());
+      assert(new RegExp(errorMessagePattern).test(match.message));
+    });
+  }
 });
